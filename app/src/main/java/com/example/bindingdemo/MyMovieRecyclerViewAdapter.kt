15094c7dbbe38a2eb86af7b95d35d4a87a5d9d0c
Chiranth <chiranth.sathyaprakash@carvana.com>
2022-03-24 23:10:29 -0700

    add binding for reycler view

--- app/src/main/java/com/example/bindingdemo/MyMovieRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/bindingdemo/MyMovieRecyclerViewAdapter.kt
@@ -5,26 +5,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bindingdemo.databinding.FragmentListDemoBinding
+import com.example.bindingdemo.databinding.FragmentListDemoListBinding
 
 class MyMovieRecyclerViewAdapter(
     private val values: List<String>
 ) : RecyclerView.Adapter<MyMovieRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.fragment_list_demo, parent, false)
-        )
+        val binding = FragmentListDemoBinding.inflate( LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.idView.text = values[position]
+        holder.binding.itemNumber.text = values[position]
+        // execute pending binding if using databinding
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
-        val idView: TextView = itemView.findViewById(R.id.item_number)
-    }
+    inner class ViewHolder(val binding: FragmentListDemoBinding) : RecyclerView.ViewHolder(binding.root)
 
 }
